cd838ff632b1041cea50bc6f476071a12282fff0
Gerald <Geraldsmeyers@gmail.com>
2025-12-10 19:31:51 +0000

    Updated PE1.py with some additional work, such as reformatting, comments, function descriptions, and complexity analysis.

--- src/Python/PE1.py
+++ src/Python/PE1.py
@@ -1,27 +1,97 @@
 """
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.</p>
-Find the sum of all the multiples of 3 or 5 below 1000.</p>
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 from collections.abc import Iterable
 
 
-def factor_set(integers: [list[int], int],
-               max_value=1000,
-               ):
-    if not isinstance(integers, Iterable):
-        integers = list(integers)
+def MlnN_factor_sum(
+    integers: Iterable[int],
+    max_value: int = 1000,
+) -> int:
+    """
+    This is a simple function that takes advantage of the fact that sets only contain unique elements.
 
-    factor_set = set()
+    Time Complexity:
+    Let I be the set of provided integers.
+    Let N = len(integers) and M = max_value.
+    Then for each n∈I, there are at most M/n multiples of n.
+
+    The total number of integers being summed is T, and there are at most 
+        T   = T1 + T2 + ... TN = M/n1 + M/n2 + ... + M/nN 
+    multiples to be summed.
+    Simplifying, 
+        T   = M(1/n1 + 1/n2 + ... + 1/nN) 
+            = M sum_{n∈I}^M 1/n.
+
+    In a worst case scenario, I is the set of integers contains every integer below N; that is I = [1, 2, ..., N].
+    Here, the total is approximated by the harmonic series:
+        T   = M sum_{n=1}^N 1/n 
+            = M int_1^M 1/x dx 
+            = M*ln(N)
+
+    ∴ Therefore the time complexity is bounded from above by 
+        O(M*ln(N)).
+
+    There is another caveat, python has to check the uniqueness of each element during a union operation, 
+        so in addition to the sum operation taking O(M*ln(N)) time, the union operation also take O(M*ln(N)) time.
+    This just adds a factor of 2 to the time complexity, and can be neglected.
+
+    Space Complexity:
+    Since this algorithm stores all mutiples of each integer below max_value then updates the set of multiples, 
+        each individual set contains M/n elements.
+    If each set were stored separately and then unioned, there would be M/n1 + M/n2 + ... + M/nN elements.
+
+    In the same worst case scenario, there would be O(M*ln(N)) space complexity.
+
+
+    :param integers: The list of integers to find all multiples of below max_value.
+    :type integers: list[int]
+    :param max_value: The bounding value to find the multiples of integers.
+    :type max_value: int
+    :return: The set of all multiples of integers below max_value.
+    :rtype: int
+    """
+
+    assert isinstance(integers, Iterable), "Provide a list of integers."
+
+    multiples_set: set[int] = set()
     for n in integers:
-        factor_set |= {i
-                       for i in range(0, max_value, n)
-                       }
-    print(factor_set)
-    return factor_set
+        multiples_set |= {
+            # starting from n, form a set of all integers
+            i for i in range(n, max_value, n)
+        }
+
+    print(multiples_set)
+    return sum(multiples_set)
 
 
-print(sum(factor_set([3, 5],
-                     1000
-                     ))
-      )
+def MxN_broken_mutiple_sum(
+    integers: Iterable[int],
+    max_value: int = 1000,
+) -> int:
+    """
+    This is a simple function that iterates through each integer in the set of integers below max_value, and checking if it is divisible by any of the integers in the set of integers.
+
+    Algorithmic Complexity:
+    Let N = len(integers) and M = max_value.
+    For each integer below M
+
+    :param integers: The list of integers to find all multiples of below max_value.
+    :type integers: list[int]
+    :param max_value: The bounding value to find the multiples of integers.
+    :type max_value: int
+    :return: The set of all multiples of integers below max_value.
+    :rtype: int
+    """
+
+    total_sum: int = 0
+    for i in range(0, max_value):
+        for n in integers:
+            if i % n == 0:
+                total_sum += i  # this will multi-count mutiples for which n is divisible by two or more elements
+                break  # exit inner for loop
+
+    return total_sum
